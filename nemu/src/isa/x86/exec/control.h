34c36271124697e2d7d99bc5e9c129998666d4ec
dzwduan <dzwduan@gmail.com>
2021-02-13 22:12:47 +0800

    del comments

--- nemu/src/isa/x86/exec/control.h
+++ nemu/src/isa/x86/exec/control.h
@@ -15,7 +15,6 @@ static inline def_EHelper(jcc) {
   // the target address is calculated at the decode stage
   //printf("exec jcc pc : 0x%x\n",cpu.pc);
   uint32_t cc = s->opcode & 0xf;
-  printf("s->opcode : 0x%x\n",s->opcode);
   //printf("cc : %d\n",cc);
   rtl_setcc(s, s0, cc);
   rtl_jrelop(s, RELOP_NE, s0, rz, s->jmp_pc);
